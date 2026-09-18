9fcfad47d2fc07291daf79e541b6c879849bac6a
Dhinesh638 <dhinesh82dj@gmail.com>
2026-09-18 21:27:44 +0530

    Time: 1 ms (97.65%) | Memory: 47.1 MB (75.94%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -5,28 +5,28 @@ class Solution {
         int[] arr=new int[2*a];
         int b=arr.length;
         int j=0;
-        // for(int i=0;i<a;i++)
-        // {
-        //     arr[i]=nums[i];
-        //     arr[a+i]=nums[i];
-        // }
-        for(int i=0;i<b;i++)
+        for(int i=0;i<a;i++)
         {
-            if(i<a)
-            {
-            arr[i]=nums[j];
-            j++;
-            }
-            else
-            {
-                if(j>a-1)
-                {
-                    j=0;
-                }
-                arr[i]=nums[j];
-                j++;
-            }
+            arr[i]=nums[i];
+            arr[a+i]=nums[i];
         }
+        // for(int i=0;i<b;i++)
+        // {
+        //     if(i<a)
+        //     {
+        //     arr[i]=nums[j];
+        //     j++;
+        //     }
+        //     else
+        //     {
+        //         if(j>a-1)
+        //         {
+        //             j=0;
+        //         }
+        //         arr[i]=nums[j];
+        //         j++;
+        //     }
+        // }
         return arr;
     }
 }
