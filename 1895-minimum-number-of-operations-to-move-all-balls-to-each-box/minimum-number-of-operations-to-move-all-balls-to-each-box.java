58f253cb306302e409d3b2bfc9fe9169c8968458
Dhinesh638 <dhinesh82dj@gmail.com>
2026-09-26 13:27:07 +0530

    Time: 132 ms (27.33%) | Memory: 46.6 MB (52.89%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,27 +1,47 @@
+// class Solution {
+//     public int[] minOperations(String boxes) {
+        
+//         int a=boxes.length();
+//         ArrayList<Integer> arr=new ArrayList<>();
+//         int[] arr1=new int[a];
+//         for(int i=0;i<a;i++)
+//         {
+//             char ch=boxes.charAt(i);
+//             if(ch=='1')
+//             {
+//                 arr.add(i);
+//             }
+//         }
+//         for(int i=0;i<a;i++)
+//         {
+//             int e=0;
+//             for(int c:arr)
+//             {
+//                 int d=Math.abs(c-i);
+//                 e=e+d;
+//             }
+//             arr1[i]=e;
+//         }
+//         return arr1;
+//     }
+// }
+
 class Solution {
     public int[] minOperations(String boxes) {
-        
+
         int a=boxes.length();
-        ArrayList<Integer> arr=new ArrayList<>();
-        int[] arr1=new int[a];
+        int[] arr=new int[a];
         for(int i=0;i<a;i++)
         {
-            char ch=boxes.charAt(i);
-            if(ch=='1')
+            for(int j=0;j<a;j++)
             {
-                arr.add(i);
+                char ch=boxes.charAt(j);
+                if(ch=='1')
+                {
+                    arr[i]=arr[i]+Math.abs(i-j);
+                }
             }
         }
-        for(int i=0;i<a;i++)
-        {
-            int e=0;
-            for(int c:arr)
-            {
-                int d=Math.abs(c-i);
-                e=e+d;
-            }
-            arr1[i]=e;
-        }
-        return arr1;
+        return arr;
     }
 }
